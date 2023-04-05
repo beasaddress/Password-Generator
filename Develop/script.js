2ff9e0969103bb34e88a17b47d2ed346933a6538
beasaddress <beasaddress@gmail.com>
2023-04-05 15:57:02 -0700

    adding prompt function to get input from user, but making sure its the right amount of characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,18 @@
-// Assignment code here
+// Assignment code here 
+var characterLength = 8;
+var choiceArr = [];
 
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +23,17 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+}
+//getting information and selected criteria from user
+//parseInt will turn the string input from the user into a number 
+function getPrompts(){
+  characterLength = parseInt(prompt("how many chacacters should we make this password? should between 8 and 128"))
+  //NaN stands for not a number....this makes sure than the user types the right thing...if they do, the if statement below should return as false
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) { //here, we are basically hoping all this is false because that means the user is inputing accepting criteria so that we can generate a password for them
+    alert("Please make sure the length is between 8 and 128 characters!!");
+    return false;
+
+  } 
+}
